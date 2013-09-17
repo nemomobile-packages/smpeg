673dcd64ae4f429497167f5593a23e9aba7ccf65
Thomas Perl <thomas.perl@jollamobile.com>
2013-09-17 18:33:38 +0200

    Split smpeg2 from sdl-mixer

--- Android.mk
+++ Android.mk
@@ -2,111 +2,42 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := SDL2_mixer
-
-# Enable this if you want to support loading MOD music via modplug
-# The library path should be a relative path to this directory.
-SUPPORT_MOD_MODPLUG := true
-MODPLUG_LIBRARY_PATH := external/libmodplug-0.8.8.4
-
-# Enable this if you want to support loading MOD music via mikmod
-# The library path should be a relative path to this directory.
-SUPPORT_MOD_MIKMOD := true
-MIKMOD_LIBRARY_PATH := external/libmikmod-3.1.12
-
-# Enable this if you want to support loading MP3 music via SMPEG
-# The library path should be a relative path to this directory.
-SUPPORT_MP3_SMPEG := true
-SMPEG_LIBRARY_PATH := external/smpeg2-2.0.0
-
-# Enable this if you want to support loading OGG Vorbis music via Tremor
-# The library path should be a relative path to this directory.
-SUPPORT_OGG := true
-OGG_LIBRARY_PATH := external/libogg-1.3.1
-VORBIS_LIBRARY_PATH := external/libvorbisidec-1.2.1
-
+LOCAL_MODULE := smpeg2
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)
-LOCAL_CFLAGS := -DWAV_MUSIC
+LOCAL_CFLAGS := -DSTDC_HEADERS=1 -DHAVE_SYS_TYPES_H=1 -DHAVE_SYS_STAT_H=1 -DHAVE_STDLIB_H=1 -DHAVE_STRING_H=1 -DHAVE_MEMORY_H=1 -DHAVE_STRINGS_H=1 -DHAVE_INTTYPES_H=1 -DHAVE_STDINT_H=1 -DHAVE_UNISTD_H=1 -DHAVE_DLFCN_H=1 -D_THREAD_SAFE -DTHREADED_AUDIO -DNOCONTROLS
 
-LOCAL_SRC_FILES := $(notdir $(filter-out %/playmus.c %/playwave.c, $(wildcard $(LOCAL_PATH)/*.c)))
+LOCAL_SRC_FILES := \
+    smpeg.cpp \
+    MPEG.cpp \
+    MPEGlist.cpp \
+    MPEGring.cpp \
+    MPEGstream.cpp \
+    MPEGsystem.cpp \
+    audio/MPEGaudio.cpp \
+    audio/bitwindow.cpp \
+    audio/filter.cpp \
+    audio/filter_2.cpp \
+    audio/hufftable.cpp \
+    audio/mpeglayer1.cpp \
+    audio/mpeglayer2.cpp \
+    audio/mpeglayer3.cpp \
+    audio/mpegtable.cpp \
+    audio/mpegtoraw.cpp \
+    video/MPEGvideo.cpp \
+    video/decoders.cpp \
+    video/floatdct.cpp \
+    video/gdith.cpp \
+    video/jrevdct.cpp \
+    video/motionvec.cpp \
+    video/parseblock.cpp \
+    video/readfile.cpp \
+    video/util.cpp \
+    video/video.cpp
 
 LOCAL_LDLIBS :=
 LOCAL_STATIC_LIBRARIES :=
 LOCAL_SHARED_LIBRARIES := SDL2
-
-ifeq ($(SUPPORT_MOD_MODPLUG),true)
-    LOCAL_C_INCLUDES += $(LOCAL_PATH)/$(MODPLUG_LIBRARY_PATH)/src $(LOCAL_PATH)/$(MODPLUG_LIBRARY_PATH)/src/libmodplug
-    LOCAL_CFLAGS += -DMODPLUG_MUSIC -DHAVE_SETENV -DHAVE_SINF
-    LOCAL_SRC_FILES += \
-        $(MODPLUG_LIBRARY_PATH)/src/fastmix.cpp \
-        $(MODPLUG_LIBRARY_PATH)/src/load_669.cpp \
-        $(MODPLUG_LIBRARY_PATH)/src/load_abc.cpp \
-        $(MODPLUG_LIBRARY_PATH)/src/load_amf.cpp \
-        $(MODPLUG_LIBRARY_PATH)/src/load_ams.cpp \
-        $(MODPLUG_LIBRARY_PATH)/src/load_dbm.cpp \
-        $(MODPLUG_LIBRARY_PATH)/src/load_dmf.cpp \
-        $(MODPLUG_LIBRARY_PATH)/src/load_dsm.cpp \
-        $(MODPLUG_LIBRARY_PATH)/src/load_far.cpp \
-        $(MODPLUG_LIBRARY_PATH)/src/load_it.cpp \
-        $(MODPLUG_LIBRARY_PATH)/src/load_j2b.cpp \
-        $(MODPLUG_LIBRARY_PATH)/src/load_mdl.cpp \
-        $(MODPLUG_LIBRARY_PATH)/src/load_med.cpp \
-        $(MODPLUG_LIBRARY_PATH)/src/load_mid.cpp \
-        $(MODPLUG_LIBRARY_PATH)/src/load_mod.cpp \
-        $(MODPLUG_LIBRARY_PATH)/src/load_mt2.cpp \
-        $(MODPLUG_LIBRARY_PATH)/src/load_mtm.cpp \
-        $(MODPLUG_LIBRARY_PATH)/src/load_okt.cpp \
-        $(MODPLUG_LIBRARY_PATH)/src/load_pat.cpp \
-        $(MODPLUG_LIBRARY_PATH)/src/load_psm.cpp \
-        $(MODPLUG_LIBRARY_PATH)/src/load_ptm.cpp \
-        $(MODPLUG_LIBRARY_PATH)/src/load_s3m.cpp \
-        $(MODPLUG_LIBRARY_PATH)/src/load_stm.cpp \
-        $(MODPLUG_LIBRARY_PATH)/src/load_ult.cpp \
-        $(MODPLUG_LIBRARY_PATH)/src/load_umx.cpp \
-        $(MODPLUG_LIBRARY_PATH)/src/load_wav.cpp \
-        $(MODPLUG_LIBRARY_PATH)/src/load_xm.cpp \
-        $(MODPLUG_LIBRARY_PATH)/src/mmcmp.cpp \
-        $(MODPLUG_LIBRARY_PATH)/src/modplug.cpp \
-        $(MODPLUG_LIBRARY_PATH)/src/snd_dsp.cpp \
-        $(MODPLUG_LIBRARY_PATH)/src/snd_flt.cpp \
-        $(MODPLUG_LIBRARY_PATH)/src/snd_fx.cpp \
-        $(MODPLUG_LIBRARY_PATH)/src/sndfile.cpp \
-        $(MODPLUG_LIBRARY_PATH)/src/sndmix.cpp
-endif
-
-ifeq ($(SUPPORT_MOD_MIKMOD),true)
-    LOCAL_C_INCLUDES += $(LOCAL_PATH)/$(MIKMOD_LIBRARY_PATH)/include
-    LOCAL_CFLAGS += -DMOD_MUSIC
-    LOCAL_SHARED_LIBRARIES += mikmod
-endif
-
-ifeq ($(SUPPORT_MP3_SMPEG),true)
-    LOCAL_C_INCLUDES += $(LOCAL_PATH)/$(SMPEG_LIBRARY_PATH)
-    LOCAL_CFLAGS += -DMP3_MUSIC
-    LOCAL_SHARED_LIBRARIES += smpeg2
-endif
-
-ifeq ($(SUPPORT_OGG),true)
-    LOCAL_C_INCLUDES += $(LOCAL_PATH)/$(OGG_LIBRARY_PATH)/include $(LOCAL_PATH)/$(VORBIS_LIBRARY_PATH)
-    LOCAL_CFLAGS += -DOGG_MUSIC -DOGG_USE_TREMOR
-    LOCAL_SRC_FILES += \
-        $(VORBIS_LIBRARY_PATH)/mdct.c \
-        $(VORBIS_LIBRARY_PATH)/block.c \
-        $(VORBIS_LIBRARY_PATH)/window.c \
-        $(VORBIS_LIBRARY_PATH)/synthesis.c \
-        $(VORBIS_LIBRARY_PATH)/info.c \
-        $(VORBIS_LIBRARY_PATH)/floor1.c \
-        $(VORBIS_LIBRARY_PATH)/floor0.c \
-        $(VORBIS_LIBRARY_PATH)/vorbisfile.c \
-        $(VORBIS_LIBRARY_PATH)/res012.c \
-        $(VORBIS_LIBRARY_PATH)/mapping0.c \
-        $(VORBIS_LIBRARY_PATH)/registry.c \
-        $(VORBIS_LIBRARY_PATH)/codebook.c \
-        $(VORBIS_LIBRARY_PATH)/sharedbook.c \
-        $(OGG_LIBRARY_PATH)/src/framing.c \
-        $(OGG_LIBRARY_PATH)/src/bitwise.c
-endif
 
 LOCAL_EXPORT_C_INCLUDES += $(LOCAL_C_INCLUDES)
 
